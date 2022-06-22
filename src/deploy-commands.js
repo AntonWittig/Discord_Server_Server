414c1d6fdf0b72c3e9ad7b17eb1bd8037787da1f
AntonWittig <anton.wittig@t-online.de>
2022-06-22 03:31:19 +0200

    added comments & reworked main files -> changed command infrastructure

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,20 +1,73 @@
-require("dotenv").config()
-//fs file system
-const fs = require("fs")
-const { REST } = require("@discordjs/rest")
-const { Routes } = require("discord-api-types/v10")
-const commands = []
+// Require the necessary environment variables
+require("dotenv").config();
 
-const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js"))
+// Require the file system modules for dynamic command creation
+const fs = require("node:fs");
+const path = require("node:path");
 
-commandFiles.forEach(commandFile => {
-    const command = require(`./commands/${commandFile}`)
-    commands.push(command.data.toJSON())
-})
+// Require the necessary discord.js classes
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
 
-const restClient =  new REST({version: "10"}).setToken(process.env.DISCORD_BOT_TOKEN)
+// Create a new rest client instance
+const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 
-restClient.put(Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID, process.env.DISCORD_GUILD_ID),
- {body: commands})
-.then(() => console.log("SUCCESS: registered commands"))
-.catch(console.error)
+// #region FUNCTIONS
+async function submitCommands(commands, guildname = "") {
+	try {
+		if (guildname) {
+			await rest.put(
+				Routes.applicationGuildCommands(process.env.APPLICATION_ID, process.env[`GUILD_ID_${guildname}`]),
+				{ body: commands },
+			);
+			console.log(`Successfully reloaded application (/) commands for guild "${guildname}".`);
+		}
+		else {
+			await rest.put(
+				Routes.applicationGlobalCommands(process.env.APPLICATION_ID),
+				{ body: commands },
+			);
+			console.log("Successfully reloaded global application (/) commands.");
+		}
+	}
+	catch (error) {
+		console.error(error);
+	}
+}
+// #endregion
+
+// #region GLOBAL COMMANDS
+// Retrieve the global command files
+const commandFilesPath = path.join(__dirname, "commands");
+const globalCommandFiles = fs.readdir(commandFilesPath).filter(file => file.endsWith(".js") && !file.startsWith("_"));
+
+// Assemble and submit the global commands
+const globalCommands = [];
+globalCommandFiles.forEach(file => {
+	const command = require(path.join(commandFilesPath, file));
+	globalCommands.push(command.data.toJSON());
+});
+submitCommands(globalCommands);
+// #endregion
+
+// #region GUILD COMMANDS
+// Retrieve the guild command folders
+const guildCommandFolders = fs.readdir(commandFilesPath, { withFileTypes: true })
+	.filter(dirent => dirent.isDirectory())
+	.map(dirent => dirent.name);
+
+// Iterate over each guild command folder
+guildCommandFolders.forEach(guildname => {
+
+	// Retrieve the guild command files
+	const guildCommandFiles = fs.readdir(path.join(commandFilesPath, guildname)).filter(file => file.endsWith(".js") && !file.startsWith("_"));
+
+	// Assemble and submit the commands
+	const guildCommands = [];
+	guildCommandFiles.forEach(file => {
+		const command = require(path.join(commandFilesPath, guildname, file));
+		guildCommands.push(command.data.toJSON());
+	});
+	submitCommands(guildCommands, guildname);
+});
+// #endregion
--- src/index.js
+++ src/index.js
@@ -1,42 +1,89 @@
-require("dotenv").config()
-const fs = require("fs")
-const { Client, Collection, Intents } = require("discord.js")
+// Require the necessary discord.js classes
+const { Client, Collection, Intents } = require("discord.js");
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS]})
-client.commands = new Collection()
+// Require the necessary environment variables
+require("dotenv").config();
 
-const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js"))
+// Require the file system modules for dynamic command creation
+const fs = require("node:fs");
+const path = require("node:path");
 
-commandFiles.forEach(commandFile => {
-    const command = require(`./commands/${commandFile}`)
-    client.commands.set(command.data.name, command)
-})
+// Create a new client instance and a commands collection
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
 
+// #region FUNCTIONS
+// Recursively retrieve files (credits to https://stackoverflow.com/users/5551941/wyattis)
+function recReadDir(dir, done) {
+	const results = [];
+	fs.readdir(dir, function(err, list) {
+		if (err) return done(err);
+		let pending = list.length;
+		if (!pending) return done(null, results);
+		list.forEach(function(file) {
+			file = path.resolve(dir, file);
+			fs.stat(file, function(err, stat) {
+				if (stat && stat.isDirectory()) {
+					recReadDir(file, function(err, res) {
+						results.push(res);
+						if (!--pending) done(null, results);
+					});
+				}
+				else {
+					results.push(file);
+					if (!--pending) done(null, results);
+				}
+			});
+		});
+	});
+}
+// #endregion
+
+// #region COMMANDS
+// Retrieve the command files
+const commandsPath = path.join(__dirname, "src", "commands");
+const commandFiles = recReadDir(commandsPath).filter((file) => file.endsWith(".js") && !file.startsWith("_"));
+
+// Assemble the commands and store them
+commandFiles.forEach((file) => {
+	const command = require(path.join(commandsPath, file));
+	client.commands.set(command.data.name, command);
+});
+// #endregion
+
+// When the client is ready, run this code (only once)
 client.once("ready", () => {
-    console.log(`Logged in as ${client.user.tag} on ${client.guilds.cache.size} server(s)`)
-    client.user.setActivity({name: "pornhub.com", type: "WATCHING"})
-})
+	console.log(
+		`Logged in as ${client.user.tag} on ${client.guilds.cache.size} server,(s)`,
+	);
+	client.user.setActivity({ name: "pornhub.com", type: "WATCHING" });
+});
 
-client.on("interaction", async (interaction) => {
-    if(!interaction.isCommand()) return
-    
-    const command = client.commands.get(interaction.commandName)
+// When an interaction with the client is created, run this code
+client.on("interactionCreate", async (interaction) => {
+	if (!interaction.isCommand()) return;
 
-    if(command) {
+	const command = client.commands.get(interaction.commandName);
 
-        try {
-            await command.execute(interaction)
-        } catch(error) {
-            console.error(error)
+	if (!command) {
+		interaction.reply("Dieser Befehl ist nicht verfügbar");
+	}
+	else {
+		try {
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(error);
 
-            if(interaction.deferred || interaction.replied){
-                interaction.editReply("Fehler beim Ausführen")
-            }else{
-                interaction.reply("Fehler beim Ausführen aufgetreten")
-            }
-        }
-    }
-})
+			if (interaction.deferred || interaction.replied) {
+				interaction.editReply("Fehler beim Ausführen");
+			}
+			else {
+				interaction.reply("Fehler beim Ausführen aufgetreten");
+			}
+		}
+	}
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN)
-
+// Login to Discord with your client's token
+client.login(process.env.BOT_TOKEN);
