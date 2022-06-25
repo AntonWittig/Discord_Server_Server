c9fd98248d49dc9e0662ad4074301b3c10159be2
AntonWittig <anton.wittig@t-online.de>
2022-06-25 18:43:00 +0200

    add commands to server

---
+++ src/commands/SERVER/flipCoin.js
@@ -0,0 +1,24 @@
+// Require the necessary discord.js class
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+// Initialize the command with a name and description
+exports.data = new SlashCommandBuilder()
+	.setName("flip_coin")
+	.setDescription("The server just flips a coin");
+
+// Execute the command
+exports.execute = async (interaction) => {
+	const toss = Math.random() * 6001;
+	if (toss == 3001) {
+		interaction.reply({
+			content: ":coin: What? that can't be! The coin landed on its **EDGE**. What could this mean?",
+			fetchReply: true,
+		}).then((message) => { message.react(":astonished:"); });
+	}
+	else if (toss <= 3000) {
+		interaction.reply(":coin: The coin landed with **HEADS** facing up.");
+	}
+	else {
+		interaction.reply(":coin: The coin landed with **TAILS** facing up.");
+	}
+};
---
+++ src/commands/SERVER/rollDice.js
@@ -0,0 +1,25 @@
+// Require the necessary discord.js class
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+// Initialize the command with a name and description
+exports.data = new SlashCommandBuilder()
+	.setName("roll_dice")
+	.setDescription("The server rolls a dice with 6 sides (standard) or you define the amount of sides (at least 1)")
+	.addIntegerOption(option =>
+		option.setName("sides")
+			.setDescription("The amount of sides")
+			.setRequired(false));
+
+// Execute the command
+exports.execute = async (interaction) => {
+	let sides = interaction.options.getInteger("sides");
+	if (sides === null) {
+		sides = 6;
+	}
+	else if (sides < 1) {
+		interaction.reply(process.env.INCORRECT_BOT_USAGE);
+		return;
+	}
+	const roll = Math.floor(Math.random() * sides) + 1;
+	interaction.reply(`:game_die: The d${sides} roll was a **${roll}**.`);
+};
