512ad6f588c54b6d4db570bebe23891c8d57b5ee
AntonWittig <anton.wittig@t-online.de>
2022-06-26 00:17:18 +0200

    try spreading to satisfy path.join

--- commands/ANTON/tictactoe.js
+++ commands/ANTON/tictactoe.js
@@ -2,13 +2,13 @@
 const path = require("node:path");
 
 // Create path to the command src
-const srcPath = [__dirname, "..", "..", "src", "commands"];
+const srcPath = [__dirname, "..", "..", "src", "commands", "tictactoe.js"];
 if (__dirname === "commands") {
 	srcPath.splice(1, 1);
 }
 
 // Require the necessary command src
-const tictactoeSrc = require(path.join(srcPath, "tictactoe.js"));
+const tictactoeSrc = require(path.join(...srcPath));
 
 // export command variables and functions
 exports.data = tictactoeSrc.data;
