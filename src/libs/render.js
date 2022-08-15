00d24b6fdfbda68a484b4ebc7b4d117f7d7994a8
AntonWittig <anton.wittig@t-online.de>
2022-08-15 18:43:36 +0200

    try reversing j values to catch a render error

--- src/libs/render.js
+++ src/libs/render.js
@@ -127,7 +127,7 @@ const chess = {
 			let renderedBoard = "";
 			if (flipped) {
 				for (let i = board.length - 1; i >= 0; i--) {
-					for (let j = board[i].length - 1; j >= 0; j--) {
+					for (let j = 0; j < board[i].length; j++) {
 						console.log("x: " + j + "; y: " + i + "; " + board[i][j]);
 						const attacked = attackedPositions.some(
 							pos => pos.includes(chess.translatePositionToNotation({ x: j, y: i })));
@@ -138,7 +138,7 @@ const chess = {
 			}
 			else {
 				for (let i = 0; i < board.length; i++) {
-					for (let j = 0; j < board[i].length; j++) {
+					for (let j = board[i].length - 1; j >= 0; j--) {
 						console.log("x: " + j + "; y: " + i + "; " + board[i][j]);
 						const attacked = attackedPositions.some(
 							pos => pos.includes(chess.translatePositionToNotation({ x: j, y: i })));
