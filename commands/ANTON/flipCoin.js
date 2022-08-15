40bae4e3584cdf8e725ea2f284b0daa99e629af1
AntonWittig <anton.wittig@t-online.de>
2022-08-15 14:21:46 +0200

    flip coin reworked

--- commands/ANTON/flipCoin.js
+++ commands/ANTON/flipCoin.js
@@ -1,24 +1,15 @@
-// Require the necessary discord.js class
-const { SlashCommandBuilder } = require("@discordjs/builders");
+// Require the path module for accessing the command src
+const path = require("node:path");
 
-// Initialize the command with a name and description
-exports.data = new SlashCommandBuilder()
-	.setName("flip_coin")
-	.setDescription("The server just flips a coin");
+// Create path to the command src
+const srcPath = [__dirname, "..", "..", "src", "commands", "flipCoin.js"];
+if (__dirname === "commands") {
+	srcPath.splice(1, 1);
+}
 
-// Execute the command
-exports.execute = async (interaction) => {
-	const toss = Math.random() * 6001;
-	if (toss == 3001) {
-		interaction.reply({
-			content: ":coin: What? that can't be! The coin landed on its **EDGE**. What could this mean?",
-			fetchReply: true,
-		}).then((message) => { message.react(":astonished:"); });
-	}
-	else if (toss <= 3000) {
-		interaction.reply(":coin: The coin landed with **HEADS** facing up.");
-	}
-	else {
-		interaction.reply(":coin: The coin landed with **TAILS** facing up.");
-	}
-};
+// Require the necessary command src
+const flipCoinSrc = require(path.join(...srcPath));
+
+// export command variables and functions
+exports.data = flipCoinSrc.data;
+exports.execute = flipCoinSrc.execute;
