8032a0e32209b56bb66f62aaef606d595c9de2d3
AntonWittig <anton.wittig@t-online.de>
2022-08-15 14:21:55 +0200

    roll dice reworked

--- commands/SERVER/rollDice.js
+++ commands/SERVER/rollDice.js
@@ -1,25 +1,15 @@
-// Require the necessary discord.js class
-const { SlashCommandBuilder } = require("@discordjs/builders");
+// Require the path module for accessing the command src
+const path = require("node:path");
 
-// Initialize the command with a name and description
-exports.data = new SlashCommandBuilder()
-	.setName("roll_dice")
-	.setDescription("The server rolls a dice with 6 sides (standard) or you define the amount of sides (at least 1)")
-	.addIntegerOption(option =>
-		option.setName("sides")
-			.setDescription("The amount of sides")
-			.setRequired(false));
+// Create path to the command src
+const srcPath = [__dirname, "..", "..", "src", "commands", "rollDice.js"];
+if (__dirname === "commands") {
+	srcPath.splice(1, 1);
+}
 
-// Execute the command
-exports.execute = async (interaction) => {
-	let sides = interaction.options.getInteger("sides");
-	if (sides === null) {
-		sides = 6;
-	}
-	else if (sides < 1) {
-		interaction.reply(process.env.INCORRECT_BOT_USAGE);
-		return;
-	}
-	const roll = Math.floor(Math.random() * sides) + 1;
-	interaction.reply(`:game_die: The d${sides} roll was a **${roll}**.`);
-};
+// Require the necessary command src
+const rollDiceSrc = require(path.join(...srcPath));
+
+// export command variables and functions
+exports.data = rollDiceSrc.data;
+exports.execute = rollDiceSrc.execute;
--- src/commands/rollDice.js
+++ src/commands/rollDice.js
@@ -1,6 +1,9 @@
 // Require the necessary discord.js class
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+// Require the monad module for handling nullable values
+const { maybe } = require("monad");
+
 // Initialize the command with a name and description
 exports.data = new SlashCommandBuilder()
 	.setName("roll_dice")
@@ -12,11 +15,8 @@ exports.data = new SlashCommandBuilder()
 
 // Execute the command
 exports.execute = async (interaction) => {
-	let sides = interaction.options.getInteger("sides");
-	if (sides === null) {
-		sides = 6;
-	}
-	else if (sides < 1) {
+	const sides = maybe(interaction.options.getInteger("sides")).getOr(6);
+	if (sides < 1) {
 		interaction.reply(process.env.INCORRECT_BOT_USAGE);
 		return;
 	}
